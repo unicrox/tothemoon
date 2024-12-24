34164ffef56201976fca819ef6c96e52e645f914
17939191890905756 <xy@amos-tech.cn>
2024-12-24 10:57:07 +0800

    coding...

---
+++ config.py
@@ -0,0 +1,9 @@
+##################################################
+
+MONGODB_HOST = "127.0.0.1"
+MONGODB_PORT = 27017
+MONGODB_DATABASE = "tothemoon"
+MONGODB_USERNAME = "tothemoon"
+MONGODB_PASSWORD = "aV3LgEbSIUtaGBCmLZ3T"
+
+##################################################
--- server.py
+++ server.py
@@ -1,7 +1,31 @@
 from sanic import Sanic
 from sanic.response import text
+import pymongo
+
+import config
 
 app = Sanic("MyHelloWorldApp")
+
+
+class DatabaseController():
+    mongo = None
+
+    def __init__(self):
+        client = pymongo.MongoClient(
+            host=config.MONGODB_HOST,
+            port=config.MONGODB_PORT, 
+            username=config.MONGODB_USERNAME, 
+            password=config.MONGODB_PASSWORD, 
+            authSource="admin",
+        )  
+        # Create the database for our example (we will use the same database throughout the tutorial
+        self.mongo = client[config.MONGODB_DATABASE]
+
+DB = None
+
+@app.listener('before_server_start')
+def serverStart(app, loop):
+    DB = DatabaseController()
 
 @app.get("/")
 async def hello_world(request):
