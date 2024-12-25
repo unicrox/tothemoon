9e66e7f18686085ed8f000ce9862599e2e993d1b
17939191890905756 <xy@amos-tech.cn>
2024-12-25 16:02:58 +0800

    simple in and out

--- server.py
+++ server.py
@@ -1,16 +1,24 @@
 from sanic import Sanic
-from sanic.response import text
+from sanic import response
 import pymongo
+import json
+from bson.objectid import ObjectId
+from datetime import datetime
 
 import config
 
-app = Sanic("MyHelloWorldApp")
+app = Sanic("ToTheMoon")
 
 
-class DatabaseController():
-    mongo = None
+class DB:
+    mongo:pymongo.MongoClient = None
 
-    def __init__(self):
+    @staticmethod
+    def init():
+        DB.mongo = DB.connectMongo()
+
+    @staticmethod
+    def connectMongo():
         client = pymongo.MongoClient(
             host=config.MONGODB_HOST,
             port=config.MONGODB_PORT, 
@@ -18,18 +26,41 @@ class DatabaseController():
             password=config.MONGODB_PASSWORD, 
             authSource="admin",
         )  
-        # Create the database for our example (we will use the same database throughout the tutorial
-        self.mongo = client[config.MONGODB_DATABASE]
+        print("-- Connected to MongoDB")
+        return client[config.MONGODB_DATABASE]
 
-DB = None
+# Ensure the result can be serialized by JSONEncoder
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, ObjectId):
+            return str(obj)  # convert ObjectId to string
+        elif isinstance(obj, datetime):
+            return obj.strftime('%Y-%m-%d %H:%M:%S')  # convert datetime to string
+        return super().default(obj)
 
+# Run something before the starting of server
 @app.listener('before_server_start')
 def serverStart(app, loop):
-    DB = DatabaseController()
+    DB.init()
 
 @app.get("/")
 async def hello_world(request):
-    return text("Hello, world.")
+    return response.text("Hello, world.")
+
+@app.route("/getServices", methods=['GET'])
+async def getServices(request):
+    services = list(DB.mongo["services"].find())
+    return response.text(json.dumps(services, cls=CustomJSONEncoder), content_type='application/json', status=200)
+
+@app.route("/setService", methods=['GET'])
+async def setService(request):
+    service = {
+        "name": "Take wedding photos",
+        "description": "test",
+        "price": 100
+    }
+    DB.mongo["services"].insert_one(service)
+    return response.text("success", content_type='application/json', status=200)
 
 
 if __name__ == '__main__':
